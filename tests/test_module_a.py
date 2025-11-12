98a678d01db5a5ab1666b00f308bd8256f2f70ab
Anton-Gurevich <antongurevich1989@gmail.com>
2025-11-13 01:24:55 +0200

    fix setup file and test

--- tests/test_module_a.py
+++ tests/test_module_a.py
@@ -1,15 +1,21 @@
 """
 This is an example test module.
 """
-import sys
-import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
-
 from module_a import hello
+from repo_b import main as repo_b_main
 
 def test_hello():
     """
     This is an example test function.
     """
     assert hello() == "Hello from module_a"
+
+
+def test_module_b_main(capsys):
+    """
+    Test the main function from module B.
+    """
+    repo_b_main()
+
+
 
