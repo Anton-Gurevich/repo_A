8148806bdc8684a659b632da2bc98a63d515bb9d
Anton-Gurevich <antongurevich1989@gmail.com>
2025-11-13 21:42:07 +0200

    test sub modules

--- tests/test_module_a.py
+++ tests/test_module_a.py
@@ -3,6 +3,7 @@ This is an example test module.
 """
 from repo_a.module_a import hello
 from repo_b import main as repo_b_main
+from repo_c import main as repo_c_main
 
 def test_hello():
     """
@@ -16,6 +17,12 @@ def test_module_b_main(capsys):
     Test the main function from module B.
     """
     repo_b_main()
+
+def test_module_c_main(capsys):
+    """
+    Test the main function from module C.
+    """
+    repo_c_main()
 
 
 
